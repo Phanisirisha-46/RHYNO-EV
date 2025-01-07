1481a4c6e0884f0753919f3f700549c5e9f679ca
siri <22501a05j2@gmail.com>
2025-01-07 11:12:06 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/RHYNO/',
   plugins: [react()],
+  base: '/RHYNO/', // Ensure Vite serves assets correctly
 });
